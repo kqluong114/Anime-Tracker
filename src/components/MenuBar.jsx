65b113f27a907264060328a28157a0d8a7d0975c
kqluong <kqluong@umich.edu>
2025-09-11 03:10:19 -0400

    Added anime card hover and scrollbars

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -58,7 +58,7 @@ function MenuBar() {
           border-b-2 border-transparent hover:border-[oklch(1_.08_174)]" to="/Profile">Profile</Link>
         </div>
         {/* Search Form */}
-        <div className="flex align-middle">
+        <div className="flex align-middle gap-1">
           <form className="relative text-black" onSubmit={handleSearch}>
             <input
               className="transition-color duration-250 border-2 border-transparent focus:rounded-b-none focus:border-[oklch(.8_.08_174)] pl-2 py-1 pr-6 rounded text-black bg-white outline-0"
@@ -75,24 +75,24 @@ function MenuBar() {
             {searchFocused && searchResults.length > 0 ? (
               <div className="absolute shadow-2xs w-full top-8 border mt-1 rounded-b-md bg-[oklch(0.35_0.04_274)]">
                 {searchResults.map((show) => (
-                  <div key={show.mal_id} className="flex justify-items-start duration-250 rounded text-xs text-bold text-white p-1 hover:bg-[oklch(0.4_0.02_274)] hover:text-[oklch(6_.08_174)] w-full">
-                    <img className="inline-block w-10 pr-1" src={show.images.jpg.image_url} alt={show.title} />
-                    <div className="inline-flex min-w-0 flex-col justify-between justify-items-start">
                       <Link
                         to={`/Anime/${show.mal_id}`}
                         onMouseDown={(e) => e.preventDefault()}>
+                  <div key={show.mal_id} className="flex justify-items-start duration-250 rounded text-xs text-bold text-white p-1 pb-2 hover:bg-[oklch(0.4_0.02_274)] hover:text-[oklch(.9_.08_174)] w-full">
+                    <img className="inline-block w-10 pr-1" src={show.images.jpg.image_url} alt={show.title} />
+                    <div className="inline-flex min-w-0 flex-col justify-between justify-items-start">
                         <span className="truncate block">{show.title}</span>
-                      </Link>
                       <div className="flex p-0 gap-0.5 w-auto text-4xs">
-                        <div className="bg-[oklch(.9_.08_174)] flex gap-1 justify-center text-black text-bold w-fit rounded-l-md p-1">
-                          <StarIcon className="w-2 content-center" /> <span className="content-center">{show.score ? show.score + "/10" : "N/A"}</span>
+                        <div className="bg-[oklch(.9_.08_174)] flex gap-1 justify-center align-middle text-black text-bold rounded-l-sm px-1 py-.5">
+                          <StarIcon className="w-2" /> <div><span className="text-[10px]">{show.score ? show.score + "/10" : "N/A"}</span></div>
                         </div>
-                        <div className="bg-[oklch(.9_.2_1)] flex gap-1 justify-center text-black text-bold w-fit rounded-r-md p-1">
-                          <FilmIcon className="w-2 content-center" /> <span className="text-100xs">{show.episodes ? show.episodes : 0}</span>
+                        <div className="bg-[oklch(.9_.2_1)] flex gap-1 justify-center text-black text-bold rounded-r-sm px-1 py-.5">
+                          <FilmIcon className="w-2" /> <div><span className="text-[10px]">{show.episodes ? show.episodes : 0}</span></div>
                         </div>
                       </div>
                     </div>
                   </div>
+                    </Link>
                 ))}
               </div>
             ) : null}
--- src/pages/AnimeSearchPage.jsx
+++ src/pages/AnimeSearchPage.jsx
@@ -105,10 +105,10 @@ function AnimeSearch() {
     }
   }
 
-  function GenreButton({children}) {
-    return <button 
-    className='border rounded-2x1 hover:bg-blue-200'
-    onClick={handleGenreClick}
+  function GenreButton({ children }) {
+    return <button
+      className='border rounded-2x1 hover:bg-blue-200'
+      onClick={handleGenreClick}
     >
       {children}
     </button>
@@ -136,25 +136,33 @@ function AnimeSearch() {
           </p>
         }
       >
-        <div className="mx-auto flex flex-wrap gap-2 justify-center">
+        <div className="mx-auto p-4 max-w-[1000px] flex flex-wrap gap-4 justify-center overflow-hidden">
           {shows.map((item) => (
-            <div key={item.mal_id} className="rounded w-[300px]">
-              <div className='relative h-3/4'>
+            <div key={item.mal_id} className="flex-1 min-w-[150px] max-w-[250px]">
+              <div className="relative h-3/4">
+              <Link to={`/anime/${item.mal_id}`}>
+                <div className="group absolute p-2 transition-all duration-200 w-full h-full  hover:bg-[oklch(.4_.08_174)]/50 hover:backdrop-blur-xl">
+                  <div className="flex flex-col gap-2 transition-all duration-200 text-xs opacity-0 text-ellipsis group-hover:opacity-100">
+                    <div>
+                      <p className="text-sm">Synopsis</p>
+                      <p className=" scrollbar scrollbar-thumb-[oklch(.9_.08_174)] overflow-auto max-h-20 text-[oklch(.9_0_0)]">{item.synopsis ? item.synopsis : 'No synopsis available.'}</p>
+                    </div>
+                    <p className="line-clamp-3">Genres: <span className="text-[oklch(.9_0_0)]">{item.genres.map(genre => genre.name).join(", ")}</span></p>
+                  </div>
+                </div>
                 <img className="w-full h-full overflow-hidden object-cover" src={item.images.jpg.image_url} alt={item.title} />
                 <div className="absolute bottom-1.5 left-1.5 flex p-0 gap-0.5 w-auto text-xs">
                   <div className="bg-[oklch(.9_.08_174)] flex gap-1 justify-center text-black text-bold w-fit rounded-l-md p-1">
-                    <StarIcon className="w-4 content-center" /> <span className="content-center">{item.score ? item.score + "/10": "N/A"}</span>
+                    <StarIcon className="w-4 content-center" /> <span className="content-center">{item.score ? item.score + "/10" : "N/A"}</span>
                   </div>
                   <div className="bg-[oklch(.9_.2_1)] flex gap-1 justify-center text-black text-bold w-fit rounded-r-md p-1">
                     <FilmIcon className="w-4 content-center" /> <span>{item.episodes ? item.episodes : 0}</span>
                   </div>
                 </div>
+              </Link>
               </div>
               <div className="py-4">
-                <Link to={`/anime/${item.mal_id}`} className="font-bold text-base mb-2 line-clamp-1">{item.title}</Link>
-                <p className="text-gray-700 text-xs">
-                  {item.synopsis ? item.synopsis.substring(0, 100) + '...' : 'No synopsis available.'}
-                </p>
+                <Link to={`/anime/${item.mal_id}`} className="transition-all duration-200 font-bold text-base mb-2 line-clamp-1 hover:text-[oklch(.9_.08_174)]">{item.title}</Link>
               </div>
             </div>
           ))}
